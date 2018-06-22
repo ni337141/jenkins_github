e3ab10b173bc7f3d04d1775a2a625c240333c659
ni337141 <nitesh.kumar31@wipro.com>
2018-06-22 23:54:19 +0530

    first commit

---
+++ github/src/test/java/Github_Jenkins_test/NewTest.java
@@ -0,0 +1,10 @@
+package Github_Jenkins_test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("I'm ready for github and jenkins");
+  }
+}
